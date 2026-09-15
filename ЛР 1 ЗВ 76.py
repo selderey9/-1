c30b26eab7bf5ce06b4636101a3dc504db1257b8
selderey9 <108930567+selderey9@users.noreply.github.com>
2026-09-15 20:39:31 +0300

    Add files via upload

---
+++ ЛР 1 ЗВ 76.py
@@ -0,0 +1,7 @@
+days=int(input("Days:"))
+hours=days*24
+minutes=days*1440
+seconds=days*86400
+print("{:<10}hours".format(hours))
+print("{:<10}minutes".format(minutes))
+print("{:<10}seconds".format(seconds))
